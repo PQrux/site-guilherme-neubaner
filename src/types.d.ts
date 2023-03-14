30c0bd0c174ef0a4441f8a964c5256147f2e9fa5
PQrux <guineub753@gmail.com>
2023-03-13 23:14:12 -0300

    added useful types and svg type declaration.

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,12 +1,49 @@
-interface Theme{
-    colors: {
+import '@emotion/react';
+
+
+declare global{
+    export interface ShadedColors{
+        primary: string;
+        error: string;
+    }
+    export interface ThemeColors extends ShadedColors{
         primary: string;
         error: string;
         text: string;
         subText: string;
-        contrastThreshold: number;
         bgPrimary: string;
         bgSecondary: string;
         bgCard: string;
     }
+    export interface ThemeConfig extends ThemeColors{
+      contrastThreshold: number;
+    }
+
+    export interface BreakpointNumbers{
+        mobile: number;
+        tablet: number;
+        laptop: number;
+        desktop: number;
+    }
+
+    export interface Breakpoints extends BreakpointNumbers{
+        mobile_query: string;
+        tablet_query: string;
+        laptop_query: string;
+        desktop_query: string;
+    }
+
+    export type AnyString = (string & {});
+    export type AnyInherit = 'inherit' | (string & {});
+    export type ColorType = keyof ThemeColors | AnyInherit;
+    export type HTMLProps<Elem extends HTMLElement> = React.DetailedHTMLProps<React.HTMLAttributes<Elem>, Elem>;
+}
+
+declare module '@emotion/react' {
+  export interface Theme{
+    colors: ThemeConfig;
+    lighterShade: ShadedColors;
+    darkerShade: ShadedColors;
+    breakpoints: Breakpoints;
+  }
 }
