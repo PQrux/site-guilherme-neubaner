8692defd01d179a4f3c1219bd409d8508cd58436
PQrux <guineub753@gmail.com>
2023-11-24 04:35:03 -0300

    solved window not defined error.

--- src/utils/user_prefered_theme/index.ts
+++ src/utils/user_prefered_theme/index.ts
@@ -2,15 +2,16 @@ import DarkTheme from "../../themes/dark";
 import LightTheme from "../../themes/light";
 
 const PREFERED_THEME_LOCALSTORAGE_KEY = 'DARK_THEME';
+const isWindowDefined = typeof window !== 'undefined';
 
 export function SetUserPreferedTheme(dark: boolean){
-    window.localStorage.setItem(PREFERED_THEME_LOCALSTORAGE_KEY, String(dark));
+    return isWindowDefined && window.localStorage.setItem(PREFERED_THEME_LOCALSTORAGE_KEY, String(dark));
 }
 
 export function GetUserPreferedTheme(){
     let dark: boolean;
     
-    const saved = window.localStorage.getItem(PREFERED_THEME_LOCALSTORAGE_KEY);
+    const saved = isWindowDefined ? window.localStorage.getItem(PREFERED_THEME_LOCALSTORAGE_KEY) : 'true';
     if(saved){
         dark = saved === 'true';
     }
